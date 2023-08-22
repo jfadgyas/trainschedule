3f65758e777bb9e77740e846e286ffe6e9d9987f
jfadgyas <djdeville72@yahoo.com>
2023-08-22 20:28:10 +0200

    v1.5

--- functions/routes/train.js
+++ functions/routes/train.js
@@ -3,14 +3,28 @@ import Train from '../models/train.js'
 
 const router = express.Router()
 
+// Get all trains
+router.get('/', async (req, res) => {
+    const trains = await Train.find()
+    res.json(trains)
+})
+
+// Get trains for 1 line
+router.get('/line/:line', async (req,res) => {
+    const trains = await Train.find({
+        line: req.params.line
+    })
+    res.json(trains)
+})
+
 // Get 1 train (and traffic around this)
 router.get('/:id', async (req, res) => {
     try{
         const train = await Train.findOne({_id: req.params.id})
         
         const destinationIndex = train.stations.length - 1
+        
         // Easier to use filter at the moment
-
         let traffic = await Train.find({
             _id: {$ne: train._id},
             line: train.line
@@ -18,10 +32,13 @@ router.get('/:id', async (req, res) => {
         traffic = traffic.filter(item => 
 
             // Starts before player && arrives after
-            ((+item.stations[0].data[2] <= +train.stations[0].data[2] &&
-            +item.stations[0].data[3] <= +train.stations[0].data[3]) &&
-            (+item.stations[destinationIndex].data[2] >= +train.stations[destinationIndex].data[2] &&
-            +item.stations[destinationIndex].data[3] >= +train.stations[destinationIndex].data[3]))
+            (
+                (+item.stations[0].data[2] <= +train.stations[0].data[2] &&
+                +item.stations[0].data[3] <= +train.stations[0].data[3])
+                &&
+                (+item.stations[destinationIndex].data[0] >= +train.stations[destinationIndex].data[0] &&
+                +item.stations[destinationIndex].data[1] >= +train.stations[destinationIndex].data[1])
+            )
             
             ||
 
